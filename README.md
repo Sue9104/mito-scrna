77d664928e04c64dc0ae5c95cb89e919029bb4f3
Sue9104 <sumin2012@163.com>
2022-12-19 22:42:28 +0800

    feat: call mito variants by snakemake

---
+++ README.md
@@ -0,0 +1,14 @@
+# Mitochondria Variant Discovery Using Snakemake
+> https://gatk.broadinstitute.org/hc/en-us/articles/4403870837275-Mitochondrial-short-variant-discovery-SNVs-Indels-
+
+## Preparation
+
+- copy `env` file to '.env' 
+- set parameters to the absolute path
+- resources for human are in `workflow/genome`
+
+
+## Run
+```sh
+  python mito_calling.py <infile> <project> [options]
+```
---
+++ mito_calling.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+import yaml
+from dotenv import dotenv_values
+import argparse
+import time
+today = time.strftime("%Y%m%d", time.localtime())
+script_path = os.path.dirname(__file__)
+parser = argparse.ArgumentParser( description='Mito Calling Pipeline')
+parser.add_argument( 'infile', type=str, help='input csv, header: sample,r1,r2')
+parser.add_argument( 'project', type=str, help='project name')
+parser.add_argument( '--outdir', type=str, help='output directory',
+                    default = f"mito-calling-{today}")
+parser.add_argument( '--cores', type=int, help='cpu cores', default = 40)
+parser.add_argument( '--env', type=str, help='env files', default = f"{script_path}/.env")
+args = parser.parse_args()
+os.makedirs(args.outdir, exist_ok=True)
+# read command line parameters
+invars = {"infile": os.path.abspath(args.infile),
+          "outdir": os.path.abspath(args.outdir),
+          "project": args.project}
+
+# read env parameters
+envdict = dotenv_values(args.env)
+
+# write all parameters into snakemake input yaml
+snake_config = os.path.join(os.path.abspath(args.outdir), "mito-calling.input.yaml")
+stream = open(snake_config, 'w')
+yaml.dump(invars | envdict, stream)
+stream.close()
+
+# run snakemake
+cmd = f"snakemake -s {script_path}/workflow/Snakefile --configfile {snake_config} -c {args.cores}"
+print(cmd)
+subprocess.run(cmd, shell=True)
